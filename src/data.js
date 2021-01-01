aff62b93cc46e4b5f0f3a2b2f88e05ef4c0f7110
Raoul Junang <63600677+jraoul2002@users.noreply.github.com>
2021-01-01 03:00:23 +0100

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* Program Data
+
+  in this file you can declare variables to store important data for your program
+  the data can only be primitives, objects or arrays
+  do not store dom elements in these variables!!!!
+    your handlers will query the DOM each time they need to make a change
+
+  these variables will be imported by your handlers when necessary
+    not by your logic
+    not by your listeners
+
+*/
+
+/**
+ * data that is saved and used between user interactionss
+ * @property {string} separator - the text to use as a "mirror"
+ */
+export const data = {
+  separator: '|',
+};
